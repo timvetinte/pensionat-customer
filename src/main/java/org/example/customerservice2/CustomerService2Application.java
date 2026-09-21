b0f7145b1d9767fe6b7008b18826d8c1b1b9bd43
timvetinte <tim.grundstrom@gmail.com>
2026-09-21 11:54:40 +0200

    Changd location of local files to correct project

---
+++ src/main/java/org/example/customerservice2/CustomerService2Application.java
@@ -0,0 +1,13 @@
+package org.example.customerservice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerService2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerService2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/customerservice2/CustomerService2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.customerservice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerService2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
